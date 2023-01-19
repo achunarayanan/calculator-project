f006f7d1281725694cf75f1bbb8b32e97cae5f55
Achu Narayanan <116958125+achunarayanan@users.noreply.github.com>
2023-01-20 01:35:46 +0530

    Add Files Via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function btnclick(val){
+    document.getElementById("screen").value=document.getElementById("screen").value+val;
+}
+function clearbtn(val2){
+    document.getElementById("screen").value=val2;
+}
