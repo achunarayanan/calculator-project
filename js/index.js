0eee260c719390f5ae2d9b20005bdc063e6c9074
Achu Narayanan <116958125+achunarayanan@users.noreply.github.com>
2023-01-20 20:29:17 +0530

    Delete js Directory

--- js/index.js
+++
@@ -1,6 +0,0 @@
-function btnclick(val){
-    document.getElementById("screen").value=document.getElementById("screen").value+val;
-}
-function clearbtn(val2){
-    document.getElementById("screen").value=val2;
-}
